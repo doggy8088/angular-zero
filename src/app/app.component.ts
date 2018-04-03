6ff206d18d8df9321a9130c8a58c067c2a8efec7
Will <doggy.huang@gmail.com>
2018-04-03 23:03:38 +0800

    學習資料繫結方法：屬性繫結 (Property Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
+  imgurl = '/assets/images/logo.png';
   constructor() {
     setTimeout(() => {
       this.title = 'The Will Will Web';
