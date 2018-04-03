079ba8daf02eba4669488839cdada7fb2768add1
Will <doggy.huang@gmail.com>
2018-04-03 15:10:10 +0800

    刪除 page2 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { Page2Component } from './page2/page2.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Page2Component
+    AppComponent
   ],
   imports: [
     BrowserModule
