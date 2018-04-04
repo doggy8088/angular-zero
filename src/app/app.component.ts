6e0d7a2f182020646ff593264b73d4a94c718c15
Will <doggy.huang@gmail.com>
2018-04-04 13:20:58 +0800

    作業 1 解決方案 - 嘗試運用 Angular 資料繫結方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,20 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  wordcount = 0;
+
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
     }
+  }
+  keywordChanges(keyword: string) {
+    this.wordcount = keyword.length;
+  }
+  keywordReset(input: HTMLInputElement) {
+    input.value = '';
+    this.wordcount = 0;
   }
 }
