f99853494c514094cc514b0526c18fbc96e38086
Will <doggy.huang@gmail.com>
2018-04-11 00:08:53 +0800

    定義 Angular 元件的輸入介面 - @Input()

--- src/app/article/article-body/article-body.component.ts
+++ src/app/article/article-body/article-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article-body',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-body.component.css']
 })
 export class ArticleBodyComponent implements OnInit {
+
+  @Input()
+  item;
 
   constructor() { }
 
