80df4c5c53a09b8faba812c4c4cf7b76b02f6fcf
Will <doggy.huang@gmail.com>
2018-04-14 19:20:03 +0800

    透過服務元件重構現有元件的程式碼

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -4,7 +4,7 @@ import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleHeaderComponent } from './article-header/article-header.component';
 import { ArticleBodyComponent } from './article-body/article-body.component';
 import { FormsModule } from '@angular/forms';
-import { DataService } from '../data.service';
+import { DataService } from './data.service';
 
 @NgModule({
   imports: [
