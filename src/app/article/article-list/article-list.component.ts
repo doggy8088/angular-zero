b936632799602db0cec19278b9d14e40bfe9e032
Will <doggy.huang@gmail.com>
2018-04-14 20:13:19 +0800

    重構 DataService 服務元件 - 回傳 Observable 物件

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -9,15 +9,41 @@ import { DataService } from '../data.service';
 export class ArticleListComponent implements OnInit {
 
   counter = 0;
+  data;
 
   constructor(public datasvc: DataService) {
   }
 
+  doDelete(item) {
+    this.datasvc.doDelete(item).subscribe(result => {
+      this.data = this.data.filter((v) => {
+        return v.id !== item.id;
+      });
+    },
+    (error) => {
+      console.log(error);
+    });
+  }
+
+  doModify(post: any) {
+    this.datasvc.doModify(post).subscribe(result => {
+      console.log(post);
+      this.data = this.data.map((item) => {
+        if (item.id == post.id) {
+          return Object.assign({}, item, post);
+        }
+        return item;
+      });
+    },
+    (error) => {
+      console.log(error);
+    });
+  }
+
   ngOnInit() {
-    this.datasvc.run();
-    setTimeout(() => {
-      this.counter++;
-    }, 2000);
+    this.datasvc.getData().subscribe(result => {
+      this.data = result;
+    })
   }
 
 }
--- src/app/article/data.service.ts
+++ src/app/article/data.service.ts
@@ -4,33 +4,22 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class DataService {
 
-  data;
-
   constructor(private http: HttpClient) {
-    http.get('http://localhost:4200/api/articles.json')
-        .subscribe(result => {
-          this.data = result;
-        });
   }
 
+  getData() {
+    return this.http.get('http://localhost:4200/api/articles.json')
+  }
   run() {
     console.log('DataService');
   }
 
   doDelete(item) {
-    this.data = this.data.filter((v) => {
-      return v.id !== item.id;
-    });
+    return this.http.delete('http://localhost:4200/api/articles/'+item.id);
   }
 
   doModify(post: any) {
-    console.log(post);
-    this.data = this.data.map((item) => {
-      if (item.id == post.id) {
-        return Object.assign({}, item, post);
-      }
-      return item;
-    })
+    return this.http.put('http://localhost:4200/api/articles/'+post.id, post);
   }
 
 }
