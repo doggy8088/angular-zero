f5cb49800610307143da9d7ffc269e6de469d5e8
Will <doggy.huang@gmail.com>
2018-04-15 13:13:04 +0800

    使用 async 管道元件訂閱 Observable 物件

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-article-list',
@@ -9,41 +10,13 @@ import { DataService } from '../data.service';
 export class ArticleListComponent implements OnInit {
 
   counter = 0;
-  data;
+  data$: Observable<any>;
 
   constructor(public datasvc: DataService) {
   }
 
-  doDelete(item) {
-    this.datasvc.doDelete(item).subscribe(result => {
-      this.data = this.data.filter((v) => {
-        return v.id !== item.id;
-      });
-    },
-    (error) => {
-      console.log(error);
-    });
-  }
-
-  doModify(post: any) {
-    this.datasvc.doModify(post).subscribe(result => {
-      console.log(post);
-      this.data = this.data.map((item) => {
-        if (item.id == post.id) {
-          return Object.assign({}, item, post);
-        }
-        return item;
-      });
-    },
-    (error) => {
-      console.log(error);
-    });
-  }
-
   ngOnInit() {
-    this.datasvc.getData().subscribe(result => {
-      this.data = result;
-    })
+    this.data$ = this.datasvc.getData();
   }
 
 }
