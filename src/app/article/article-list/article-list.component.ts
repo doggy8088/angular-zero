5a3b64b43bf038eacdb5630bd0ebde6f95998b29
Will <doggy.huang@gmail.com>
2018-07-12 22:43:33 +0800

    Upgrade to Angular v6 (Including CLI)

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-article-list',
